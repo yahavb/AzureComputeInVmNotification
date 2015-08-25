268816ddf7baa090b4d0fecb91416d8e3114a2e2
Yahav Biran <ybiran@colostate.edu>
2015-08-25 11:17:41 -0700

    Create IsVmInMaint.ps1

---
+++ samples/IsVmInMaint.ps1
@@ -0,0 +1,6 @@
+# This sample uses the native powershell functionality for pulling dynamic metadata from a Windows Azure VM for incoming reboot events caused by planned maintenance. 
+# When reboot is pending, it will log an event in the EventLog.
+# Before executing it, the event needs to be registered in the EventLogger by executing: New-EventLog –LogName Application –Source “IsVmInMaint”
+
+$result=curl http://169.254.169.254/metadata/v1/InstanceInfo | findstr "^Content" | findstr -i “reboot”
+if ($result) {Write-EventLog -LogName Application –Source "IsVmInMaint" -EntryType Information –EventID 1 –Message "Incoming VM reboot"}
