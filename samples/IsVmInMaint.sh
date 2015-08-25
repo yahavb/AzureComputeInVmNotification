e2103a5bec1edaeebcaac22c35175fb1be8dd7a8
Yahav Biran <ybiran@colostate.edu>
2015-08-25 14:25:33 -0700

    Add the crontab line for 5 min execution

--- samples/IsVmInMaint.sh
+++ samples/IsVmInMaint.sh
@@ -1,6 +1,7 @@
 # This sample uses the native bash functionality for pulling dynamic metadata from a Windows Azure VM for incoming reboot events caused by planned maintenance. 
 # When reboot is pending, it will log an event in the Linux system log using the syslog protocol.
-
+# Add the following line to your local crontab. It will execute the script every 5 minutes 
+# */5 * * * * /home/user/IsVmInMaint.sh
 #!/bin/bash
 result=`curl http://169.254.169.254/metadata/v1/InstanceInfo| grep -i ud`
 if [ -n $result ]; then
