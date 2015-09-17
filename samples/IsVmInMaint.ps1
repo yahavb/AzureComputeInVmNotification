346bcb9e0ee814cad8d406b2a100a0c2ecd0063a
Yahav Biran <ybiran@colostate.edu>
2015-09-17 11:26:42 -0700

    adopt to /metadata URI

--- samples/IsVmInMaint.ps1
+++ samples/IsVmInMaint.ps1
@@ -2,5 +2,5 @@
 # When reboot is pending, it will log an event in the EventLog.
 # Before executing it, the event needs to be registered in the EventLogger by executing: New-EventLog –LogName Application –Source “IsVmInMaint”
 
-$result=curl http://169.254.169.254/metadata/v1/InstanceInfo | findstr "^Content" | findstr -i “reboot”
+$result=curl http://169.254.169.254/metadata/v1/InstanceInfo | findstr "^Content" | findstr -i EventID
 if ($result) {Write-EventLog -LogName Application –Source "IsVmInMaint" -EntryType Information –EventID 1 –Message "Incoming VM reboot"}
